a25e87b6cc7b4f9ec2bce6bf78f1d0e7f008d784
deng bowen <130885801+Db-hurricane@users.noreply.github.com>
2023-06-09 22:09:08 +0800

    Add files via upload

--- graph.h
+++ graph.h
@@ -1,265 +1,442 @@
-#include<stdio.h>
-#include<string.h>
-#include<malloc.h>
+#include <cstdio>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+using namespace std;
 
-#define Max 50
-
+#define Max 100
+#define INT_MAX 2147483647
 
 typedef struct Transport
 {
-    char StartCity[20];
-    char EndCity[20];
     int cost;
     int SpendTime;
-    int transform;//0表示火车，1表示飞机
-};
+    int transform; // 0表示火车，1表示飞机
+} Transport;
 
 typedef struct Route
 {
     int tail, head;
-
+    Transport transport;
     struct Route* tlink, * hlink;
-}route;
+} Route;
 
 typedef struct City
 {
     char name[20];
-    //struct Transport* transports=malloc(numbers_of*sizeof(struct Transport));
     Route* firstin, * firstout;
-}city;
+} City;
 
 typedef struct Graph
 {
     City cities[Max];
-    int citynum, routnum;
-}map;
+    int citynum, routenum;
+} Map;
 
-//初始化图的十字链表
-void InitMap(Graph* map)
-{
+// 初始化图的十字链表
+Map* initGraph() {
+    Map* map = (Map*)malloc(sizeof(Map));
+    if (map == NULL) {
+        return NULL;
+    }
     map->citynum = 0;
-    map->routnum = 0;
-    for (int i = 0; i < Max; i++)
-    {
-        map->cities[i].firstin = NULL;
-        map->cities[i].firstout = NULL;
-    };
+    map->routenum = 0;
+    return map;
 }
 
-//初始化交通图
-void InitOListDG(map* G)
-{
-    int i, j, k;
-    char name[20];
-    printf("请输入城市数量：");
-    scanf_s("%d", &G->citynum);
-    printf("请输入城市名称：");
-    for (i = 0; i < G->citynum; i++)
-    {
-        scanf_s("%s", name);
-        strcpy_s(G->cities[i].name, name);
+// 添加城市
+int addCity(Map* map, const char* cityName) {
+    if (map->citynum >= Max) {
+        return -1; // 达到城市数量上限
     }
-    printf("请输入路线数量：");
-    scanf_s("%d", &G->routnum);
-    printf("请输入路线信息：");
-    for (k = 0; k < G->routnum; k++)
-    {
-        Transport* transport = (Transport*)malloc(sizeof(Transport));
-        scanf_s("%s %s %d %d %d", transport->StartCity, transport->EndCity, &transport->cost, &transport->SpendTime, &transport->transform);
-        for (i = 0; i < G->citynum; i++)
-        {
-            if (strcmp(G->cities[i].name, transport->StartCity) == 0)
+    // 检查是否已经存在同名城市
+    strncpy(map->cities[map->citynum].name, cityName, 20);
+    map->cities[map->citynum].firstin = NULL;
+    map->cities[map->citynum].firstout = NULL;
+    map->citynum++;
+    return 0; // 添加成功
+}
+
+// 删除城市（按名称删除）
+int deleteCity(Map* map, const char* cityName) {
+    for (int i = 0; i < map->citynum; i++) {
+        if (strncmp(map->cities[i].name, cityName, 20) == 0) {
+            // 找到城市，删除操作通常需要处理的部分比较复杂，需要考虑城市间的路线等问题
+            // 这里只简单地通过将最后一个城市移到被删除的位置来进行删除
+            map->cities[i] = map->cities[map->citynum - 1];
+            map->citynum--;
+            return 0; // 删除成功
+        }
+    }
+    return -1; // 未找到城市
+}
+
+// 添加路线（这里只添加单向路线，双向路线需要调用两次）
+int addRoute(Map* map, const char* startCityName, const char* endCityName, Transport transport) {
+    Route* route = (Route*)malloc(sizeof(Route));
+    if (route == NULL) {
+        return -1; // 内存分配失败
+    }
+    // 找到起始城市和终止城市的索引
+    int startCityIndex = -1, endCityIndex = -1;
+    for (int i = 0; i < map->citynum; i++) {
+        // 这里假设城市名称不会重复
+        if (strncmp(map->cities[i].name, startCityName, 20) == 0) {
+            startCityIndex = i;
+        }
+        else if (strncmp(map->cities[i].name, endCityName, 20) == 0) {
+            endCityIndex = i;
+        }
+    }
+    // 如果未找到城市，需要释放内存
+    if (startCityIndex == -1 || endCityIndex == -1) {
+        free(route);
+        return -1;
+    }
+    // 这里假设路线不存在，如果路线已存在，需要先删除原有路线
+    route->head = endCityIndex;
+    route->tail = startCityIndex;
+    route->transport = transport;
+    route->tlink = map->cities[startCityIndex].firstout;
+    route->hlink = map->cities[endCityIndex].firstin;
+    map->cities[startCityIndex].firstout = route;
+    map->cities[endCityIndex].firstin = route;
+    map->routenum++;
+    return 0; // 添加成功
+}
+
+// 删除路线（按起始城市和终止城市删除）
+int deleteRoute(Map* map, const char* startCityName, const char* endCityName) {
+    // 找到起始城市和终止城市的索引
+    int startCityIndex = -1, endCityIndex = -1;
+    for (int i = 0; i < map->citynum; i++) {
+        if (strncmp(map->cities[i].name, startCityName, 20) == 0) {
+            startCityIndex = i;
+        }
+        else if (strncmp(map->cities[i].name, endCityName, 20) == 0) {
+            endCityIndex = i;
+        }
+    }
+    if (startCityIndex == -1 || endCityIndex == -1) {
+        return -1; // 未找到城市
+    }
+    // 找到路线并删除
+    Route* current = map->cities[startCityIndex].firstout;
+    Route* prev = NULL;
+    // 这里假设路线不存在
+    while (current != NULL && current->head != endCityIndex) {
+        prev = current;
+        current = current->tlink;
+    }
+    if (current == NULL) {
+        return -1; // 未找到路线
+    }
+    // 删除路线
+    if (prev == NULL) {
+        map->cities[startCityIndex].firstout = current->tlink;
+    }
+    else {
+        prev->tlink = current->tlink;
+    }
+    prev = NULL;
+    // 这里假设路线不存在
+    Route* temp = map->cities[endCityIndex].firstin;
+    while (temp != current) {
+        prev = temp;
+        temp = temp->hlink;
+    }
+    // 删除路线
+    if (prev == NULL) {
+        map->cities[endCityIndex].firstin = current->hlink;
+    }
+    else {
+        prev->hlink = current->hlink;
+    }
+    free(current);
+    map->routenum--;
+    return 0; // 删除成功
+}
+
+
+// 从文件中读取城市和路线信息
+int loadGraph(Map* map, const char* filename) {
+    FILE* file;
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        return -1; // 打开文件失败
+    }
+    char line[100];
+    while (fgets(line, 100, file) != NULL) {
+        // 这里假设每行的格式如下：
+        // 对于城市：城市名
+        // 对于路线：起始城市,终止城市,交通工具,费用,所需时间
+        if (strchr(line, ',') == NULL) {
+            // 这是一个城市
+            line[strcspn(line, "\n")] = 0; // 去掉换行符
+            addCity(map, line);
+        }
+        else {
+            // 这是一条路线
+            char startCity[20], endCity[20];
+            Transport transport;
+            // 这里假设路线信息格式正确
+            sscanf(line, "%[^,],%[^,],%d,%d,%d\n", startCity, endCity, &transport.transform, &transport.cost, &transport.SpendTime);
+            addRoute(map, startCity, endCity, transport);
+        }
+    }
+    fclose(file);
+    return 0;
+}
+
+// 将城市和路线信息写入文件
+int saveGraph(Map* map, const char* filename) {
+    FILE* file;
+    file = fopen(filename, "w");
+    if (file == NULL) {
+        return -1; // 打开文件失败
+    }
+    for (int i = 0; i < map->citynum; i++) {
+        // 写入城市信息
+        fprintf(file, "%s\n", map->cities[i].name);
+    }
+    for (int i = 0; i < map->citynum; i++) {
+        // 写入路线信息
+        Route* route = map->cities[i].firstout;
+        while (route != NULL) {
+            // 这里假设路线信息格式正确
+            fprintf(file, "%s,%s,%d,%d,%d\n", map->cities[route->tail].name, map->cities[route->head].name, route->transport.transform, route->transport.cost, route->transport.SpendTime);
+            route = route->tlink;
+        }
+    }
+    fclose(file);
+    return 0;
+}
+
+//根据权重选择最小的路线
+void Dijkstra(Map* map, int start, int end, int* dist, int* prev, const int buy_transform, int& total_time, int& total_cost) {
+    int visited[Max]; // 记录城市是否被访问过
+    // 初始化数据
+    for (int i = 0; i < map->citynum; ++i) {
+        visited[i] = 0; // 初始状态下，所有城市都未被访问过
+        dist[i] = INT_MAX; // 所有城市到开始城市的距离初始化为无穷大
+    }
+
+    // 对于开始城市，距离为0
+    dist[start] = 0;
+    total_time = 0;
+    total_cost = 0;
+    for (int i = 0; i < map->citynum; ++i) {
+        int u = -1; // 寻找当前未访问城市中距离最小的城市
+        int min = INT_MAX;
+        // 找到距离最小的城市
+        for (int j = 0; j < map->citynum; ++j) {
+            if (!visited[j] && dist[j] < min) {
+                u = j;
+                min = dist[j];
+            }
+        }
+
+        if (u == -1) // 所有城市已访问完
+            return;
+
+        visited[u] = 1; // 标记城市已被访问
+        Route* route = map->cities[u].firstout; // 获取该城市所有出边
+        while (route) {
+            if (route->transport.transform == buy_transform)
             {
-                for (j = 0; j < G->citynum; j++)
+                int v = route->head; // 获取边的另一端城市
+                if (!visited[v] && route->transport.SpendTime != INT_MAX && dist[u] + route->transport.SpendTime < dist[v])
                 {
-                    if (strcmp(G->cities[j].name, transport->EndCity) == 0)
-                    {
-                        Route* p = (Route*)malloc(sizeof(Route));
-                        p->head = j;
-                        p->tail = i;
-                        p->tlink = G->cities[i].firstout;
-                        p->hlink = G->cities[j].firstin;
-                        G->cities[i].firstout = p;
-                        G->cities[j].firstin = p;
-                    }
+                    // 如果新的路径比原有的路径短，更新路径信息
+                    dist[v] = dist[u] + route->transport.SpendTime;
+                    prev[v] = u;
                 }
             }
+
+            route = route->tlink; // 移动到下一个边
+        }
+
+    }
+    // 找到最短路径后，更新总时间和总花费
+    total_time = dist[end];
+    int cur = end;
+    while (cur != start) {
+        Route* route = map->cities[prev[cur]].firstout;
+        while (route) {
+            if (route->head == cur) {
+                total_cost += route->transport.cost;
+                break;
+            }
+            route = route->tlink;
+        }
+        cur = prev[cur];
+    }
+}
+
+//获取图中城市的位置
+int getCityIndex(Map* map, const char* cityName) {
+    for (int i = 0; i < map->citynum; i++) {
+        if (strcmp(map->cities[i].name, cityName) == 0) {
+            return i;
+        }
+    }
+    return -1;  //返回-1表示没有找到该城市
+}
+
+//根据选择的交通方式，最快的路线
+Route* fastestRoute(Map* map, const char* startCityName, const char* endCityName,int transform)
+{
+    int start = getCityIndex(map, startCityName);
+    int end = getCityIndex(map, endCityName);
+    int dist[Max], prev[Max];
+    int total_time, total_cost;
+    
+    Dijkstra(map, start, end, dist, prev, transform, total_time, total_cost);
+    // 如果终点城市的距离为无穷大，说明没有从起点到终点的路线
+    if (dist[end] == INT_MAX) {
+        printf("没有从%s到%s的路线.\n", startCityName, endCityName);
+        return NULL;
+    }
+
+    printf("从%s到%s的最快路线花费%d小时%d元钱.\n", startCityName, endCityName, total_time,total_cost);
+
+    // 打印路径
+    int path[Max], cnt = 0;
+    for (int i = end; i != start; i = prev[i])
+        path[cnt++] = i;
+    path[cnt] = start;
+
+    for (int i = cnt; i > 0; --i) {
+        printf("%s -> ", map->cities[path[i]].name);
+    }
+    printf("%s\n", map->cities[path[0]].name);
+
+    return NULL;
+}
+
+//根据选择的交通方式，最便宜的路线
+Route* cheapestRoute(Map* map, const char* startCityName, const char* endCityName,int transform) {
+    int start = getCityIndex(map, startCityName);
+    int end = getCityIndex(map, endCityName);
+    int dist[Max], prev[Max];
+    int total_time,total_cost;
+
+    Dijkstra(map, start, end, dist, prev, transform, total_time, total_cost);
+    // 如果终点城市的距离为无穷大，说明没有从起点到终点的路线
+    if (dist[end] == INT_MAX) {
+        printf("没有从%s到%s的路线.\n", startCityName, endCityName);
+        return NULL;
+    }
+
+    printf("从%s到%s的最便宜路线花费%d小时%d元钱.\n", startCityName, endCityName, total_time, total_cost);
+
+    // 打印路径
+    int path[Max], cnt = 0;
+    for (int i = end; i != start; i = prev[i])
+        path[cnt++] = i;
+    path[cnt] = start;
+
+    for (int i = cnt; i > 0; --i) {
+        printf("%s -> ", map->cities[path[i]].name);
+    }
+    printf("%s\n", map->cities[path[0]].name);
+
+    return NULL;
+}
+
+
+//根据选择的交通方式，选择最少中转的路线
+Route* leastTransfersRoute(Map* map, const char* startCityName, const char* endCityName,int transform) {
+    int start = getCityIndex(map, startCityName);
+    int end = getCityIndex(map, endCityName);
+    int dist[Max], prev[Max];
+    int total_time = 0, total_cost = 0;
+    for (int i = 0; i < map->citynum; i++) {
+        dist[i] = INT_MAX;  // 初始化所有城市的中转次数为无穷大
+    }
+    dist[start] = 0;  // 起点城市的中转次数为0
+
+    // 使用队列实现BFS
+    int queue[Max];
+    int head = 0, tail = 0;
+    queue[tail++] = start;  // 起点城市入队
+
+    while (head < tail) {
+        int u = queue[head++];  // 出队
+        Route* route = map->cities[u].firstout;
+        // 遍历所有从u出发的边
+        while (route) {
+            // 如果边的另一端城市的中转次数大于u的中转次数加1，更新中转次数
+            if(route->transport.transform==transform)
+            {
+                int v = route->head;
+                if (dist[u] + 1 < dist[v]) {  // 如果新的中转次数比原有的中转次数少，更新中转次数
+                    dist[v] = dist[u] + 1;
+                    prev[v] = u;
+                    total_time += route->transport.SpendTime;
+                    total_cost += route->transport.cost;
+                    queue[tail++] = v;  // 新的城市入队
+                }
+            }
+            // 下一条边
+            route = route->tlink;
+        }
+    }
+    // 如果终点城市的中转次数为无穷大，说明没有从起点到终点的路线
+    if (dist[end] == INT_MAX) {
+        printf("No available route from %s to %s.\n", startCityName, endCityName);
+        return NULL;
+    }
+
+    printf("从%s到%s的最少中转路线中转%d次.总共花费了%d小时时间，%d元钱\n", startCityName, endCityName, dist[end],total_time,total_cost);
+
+    // 打印路径
+    int path[Max], cnt = 0;
+    for (int i = end; i != start; i = prev[i])
+        path[cnt++] = i;
+    path[cnt] = start;
+    // 逆序打印路径
+    for (int i = cnt; i > 0; --i) {
+        printf("%s -> ", map->cities[path[i]].name);
+    }
+    printf("%s\n", map->cities[path[0]].name);
+
+    return NULL;
+}
+
+//展示所有的城市
+void dispalycity(Map* map)
+{
+    int count = 0;
+	for (int i = 0; i < map->citynum; i++)
+	{
+		printf("%s\t", map->cities[i].name);
+        count++;
+        if(count%5==0)
+        {
+	        printf("\n");
+        }
+	}
+}
+
+// 打印所有火车和飞机的路线
+void PrintAllRoutes(Map* map) {
+    // 遍历所有的城市
+    for (int i = 0; i < map->citynum; ++i) {
+        Route* route = map->cities[i].firstout; // 获取该城市的所有出边
+        // 遍历所有的路线
+        while (route) {
+            // 如果是火车或飞机的路线，就打印出来
+            if (route->transport.transform == 0) {
+                printf("火车路线: %s --> %s, 花费时间: %d, 花费费用: %d\n",
+                    map->cities[i].name, map->cities[route->head].name, route->transport.SpendTime, route->transport.cost);
+            }
+            else if (route->transport.transform == 1) {
+                printf("飞机路线: %s --> %s, 花费时间: %d, 花费费用: %d\n",
+                    map->cities[i].name, map->cities[route->head].name, route->transport.SpendTime, route->transport.cost);
+            }
+            route = route->tlink; // 移动到下一个边
         }
     }
 }
-
-//添加城市
-void AddCity(map* G)
-{
-	int i, j;
-	char name[20];
-	printf("请输入城市名称：");
-	scanf_s("%s", name);
-	strcpy_s(G->cities[G->citynum].name, name);
-	G->citynum++;
-	printf("请输入路线数量：");
-	scanf_s("%d", &j);
-	printf("请输入路线信息：");
-	for (i = 0; i < j; i++)
-	{
-		Transport* transport = (Transport*)malloc(sizeof(Transport));
-		scanf_s("%s %s %d %d %d", transport->StartCity, transport->EndCity, &transport->cost, &transport->SpendTime, &transport->transform);
-		for (i = 0; i < G->citynum; i++)
-		{
-			if (strcmp(G->cities[i].name, transport->StartCity) == 0)
-			{
-				for (j = 0; j < G->citynum; j++)
-				{
-					if (strcmp(G->cities[j].name, transport->EndCity) == 0)
-					{
-						Route* p = (Route*)malloc(sizeof(Route));
-						p->head = j;
-						p->tail = i;
-						p->tlink = G->cities[i].firstout;
-						p->hlink = G->cities[j].firstin;
-						G->cities[i].firstout = p;
-						G->cities[j].firstin = p;
-					}
-				}
-			}
-		}
-	}
-}
-
-//删除城市
-void DeleteCity(map* G)
-{
-	int i, j;
-	char name[20];
-	printf("请输入城市名称：");
-	scanf_s("%s", name);
-	for (i = 0; i < G->citynum; i++)
-	{
-		if (strcmp(G->cities[i].name, name) == 0)
-		{
-			for (j = 0; j < G->citynum; j++)
-			{
-				if (strcmp(G->cities[j].name, name) == 0)
-				{
-					Route* p = G->cities[i].firstout;
-					while (p != NULL)
-					{
-						if (p->head == j)
-						{
-							p->tail = -1;
-							p->head = -1;
-						}
-						p = p->tlink;
-					}
-					p = G->cities[j].firstin;
-					while (p != NULL)
-					{
-						if (p->tail == i)
-						{
-							p->tail = -1;
-							p->head = -1;
-						}
-						p = p->hlink;
-					}
-				}
-			}
-		}
-	}
-}
-
-
-//添加路线
-void AddRoute(map* G)
-{
-	int i, j;
-	printf("请输入路线数量：");
-	scanf_s("%d", &j);
-	printf("请输入路线信息：");
-	for (i = 0; i < j; i++)
-	{
-		Transport* transport = (Transport*)malloc(sizeof(Transport));
-		scanf_s("%s %s %d %d %d", transport->StartCity, transport->EndCity, &transport->cost, &transport->SpendTime, &transport->transform);
-		for (i = 0; i < G->citynum; i++)
-		{
-			if (strcmp(G->cities[i].name, transport->StartCity) == 0)
-			{
-				for (j = 0; j < G->citynum; j++)
-				{
-					if (strcmp(G->cities[j].name, transport->EndCity) == 0)
-					{
-						Route* p = (Route*)malloc(sizeof(Route));
-						p->head = j;
-						p->tail = i;
-						p->tlink = G->cities[i].firstout;
-						p->hlink = G->cities[j].firstin;
-						G->cities[i].firstout = p;
-						G->cities[j].firstin = p;
-					}
-				}
-			}
-		}
-	}
-}
-
-
-//删除路线
-void DeleteRoute(map* G)
-{
-	int i, j;
-	char name[20];
-	printf("请输入路线信息：");
-	scanf_s("%s %s", name);
-	for (i = 0; i < G->citynum; i++)
-	{
-		if (strcmp(G->cities[i].name, name) == 0)
-		{
-			for (j = 0; j < G->citynum; j++)
-			{
-				if (strcmp(G->cities[j].name, name) == 0)
-				{
-					Route* p = G->cities[i].firstout;
-					while (p != NULL)
-					{
-						if (p->head == j)
-						{
-							p->tail = -1;
-							p->head = -1;
-						}
-						p = p->tlink;
-					}
-					p = G->cities[j].firstin;
-					while (p != NULL)
-					{
-						if (p->tail == i)
-						{
-							p->tail = -1;
-							p->head = -1;
-						}
-						p = p->hlink;
-					}
-				}
-			}
-		}
-	}
-}
-
-//从文件中读取城市和路线信息
-
-
-//将城市和路线信息写入文件
-
-
-//查询最快路线
-
-
-//查询最省钱路线
-
-
-//查询最少中转次数路线
-
-
-//购买火车/飞机票
-
-
-//显示菜单并获取用户输入
 
--- main.cpp
+++ main.cpp
@@ -1,41 +1,189 @@
 #include"graph.h"
+#include <time.h>
+
+void initMap(Map* map) {
+	// 城市名字列表
+	const char* cityNames[15] = { "北京", "上海", "广州", "深圳", "成都", "杭州", "西安",
+								 "长沙", "南京", "武汉", "天津", "重庆", "济南", "长春", "郑州" };
+	// 添加城市
+	for (int i = 0; i < 15; i++) {
+		addCity(map, cityNames[i]);
+	}
+
+	// 添加路线，使用随机生成的时间和成本
+	srand(time(0));  // 用当前时间作为随机数种子
+	for (int i = 0; i < 15; i++) {
+		for (int j = 0; j < 3; j++) {
+			int endCityIndex = rand() % 15;  // 随机选择目标城市
+			int trans=rand() % 2;  // 随机选择交通方式
+			if (endCityIndex == i) {
+				continue;  // 如果目标城市就是起点城市，跳过
+			}
+			if(trans==0)
+			{
+				// 添加火车路线
+				Transport train;
+				train.transform = 0;
+				train.SpendTime = rand() % 24 + 6;  // 随机生成12-36小时的旅行时间
+				train.cost = rand() % 500 + 200;  // 随机生成200-700元的旅行成本
+				addRoute(map, cityNames[i], cityNames[endCityIndex], train);
+			}
+			if(trans==1)
+			{
+				// 添加飞机路线
+				Transport plane;
+				plane.transform = 1;
+				plane.SpendTime = rand() % 5 + 1;  // 随机生成2-7小时的旅行时间
+				plane.cost = rand() % 1500 + 500;  // 随机生成500-2000元的旅行成本
+				addRoute(map, cityNames[i], cityNames[endCityIndex], plane);
+			}
+		}
+	}
+}
+
+
 
 int main()
 {
-	Graph map;
-	InitMap(&map);
+	Graph* map = initGraph();
+	initMap(map);
+	dispalycity(map);
+	PrintAllRoutes(map);
 	int choice;
 	do {
 		printf("\n**********交通咨询系统**********\n");
 		printf("1.添加城市\n");
-		printf("2.添加火车路线\n");
-		printf("3.添加飞机路线\n");
-		printf("4.购买火车票\n");
-		printf("5.购买飞机票\n");
+		printf("2.添加路线\n");
+		printf("3.删除城市\n");
+		printf("4.删除路线\n");
+		printf("5.购买车票\n");
 		printf("6.用文件导入路线\n");
-		printf("7.退出\n");
+		printf("7.用文件导出路线\n");
+		printf("8.显示所有路线\n");
+		printf("9.退出\n");
 		printf("*********************************\n");
 		printf("请输入你的选择：");
-		scanf_s("%d", &choice);
+		scanf("%d", &choice);
 		switch (choice)
 		{
 		case 1:
-			break;
+			{
+				printf("请输入城市名称：\n");
+				char add_cityname[50];
+				fflush(stdin);
+				cin >> add_cityname;
+				addCity(map,add_cityname);
+				break;
+			}
 		case 2:
-			break;
+			{
+				printf("请输入起点城市名称：\n");
+				char startcity[50];
+				fflush(stdin);
+				cin >> startcity;
+				printf("请输入终点城市名称：\n");
+				char endcity[50];
+				fflush(stdin);
+				cin >> endcity;
+				printf("请输入交通方式：（0为火车，1为飞机）\n");
+				Transport transtortform;
+				scanf("%d", &transtortform.transform);
+				printf("请输入花费的时间\n");
+				scanf("%d", &transtortform.SpendTime);
+				printf("请输入花费的钱\n");
+				scanf("%d", &transtortform.cost);
+				addRoute(map, startcity, endcity,transtortform);
+				break;
+			}
 		case 3:
+			{
+				printf("请输入城市名称：\n");
+				char del_cityname[50];
+				fflush(stdin);
+				cin >> del_cityname;
+				deleteCity(map, del_cityname);
+			}
 			break;
 		case 4:
-			break;
+			{
+				printf("请输入删除的起点城市名称：\n");
+				char del_startcity[50];
+				fflush(stdin);
+				cin >> del_startcity;
+				printf("请输入删除的终点城市名称：\n");
+				char del_endcity[50];
+				fflush(stdin);
+				cin >> del_endcity;
+				deleteRoute(map, del_startcity, del_endcity);
+				break;
+			}
 		case 5:
-			break;
+			{
+				printf("请输入起点城市名称：\n");
+				char buy_startcity[50];
+				fflush(stdin);
+				cin >> buy_startcity;
+				printf("请输入终点城市名称：\n");
+				char buy_endcity[50];
+				fflush(stdin);
+				cin >> buy_endcity;
+				printf("请输入交通方式：（0为火车，1为飞机）\n");
+				int buy_transform;
+				scanf("%d", &buy_transform);
+				printf("请选择最快路线，最便宜路线，最少中转路线：（1为最快，2为最便宜，3为最少中转）\n");
+				int trans_choice;
+				scanf("%d", &trans_choice);
+				if(trans_choice==1)
+				{
+					fastestRoute(map, buy_startcity, buy_endcity, buy_transform);
+				}
+				else if(trans_choice==2)
+				{
+					cheapestRoute(map, buy_startcity, buy_endcity, buy_transform);
+				}
+				else if(trans_choice==3)
+				{
+					leastTransfersRoute(map, buy_startcity, buy_endcity, buy_transform);
+				}
+				else
+				{
+					printf("输入错误，请重新输入！\n");
+				}
+				break;
+			}
 		case 6:
-			break;
+			{
+				printf("请输入文件名：\n");
+				char filename[50];
+				fflush(stdin);
+				cin >> filename;
+				loadGraph(map, filename);
+				break;
+			}
 		case 7:
+			{
+				printf("请输入文件名：\n");
+				char filename[50];
+				fflush(stdin);
+				cin >> filename;
+				saveGraph(map, filename);
+				break;
+			}
+		case 8:
+			{
+				PrintAllRoutes(map);
+				break;
+			}
+		case 9:
+			{
+			choice = 9;
 			break;
+			}
 		default:
-			printf("无效的输入，请重新输入\n");
+			{
+				printf("输入错误，请重新输入！\n");
+			}
 		}
-	} while (choice == 0);
+	} while (choice != 9);
 	return 0;
 }
